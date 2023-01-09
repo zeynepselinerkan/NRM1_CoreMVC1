668ac97c8cf2c9ce6127c239cf75401d1623b2d5
zeynepselinerkan <Q2MCSUGt7ZjRYex>
2023-01-09 03:50:07 +0300

    Add project files.

---
+++ NRM1_CoreMVC1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NRM1_CoreMVC1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NRM1_CoreMVC1.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        List<Employee> List = new List<Employee>();
+
+        public IActionResult Index()
+        {
+            Employee e = new Employee();
+            e.FirstName = "Ali";
+            e.LastName = "Veli";
+            List.Add(e);
+
+            Employee e1 = new Employee();
+            e1.FirstName = "Osman";
+            e1.LastName = "Veli";
+            List.Add(e1);
+
+            Employee e2 = new Employee();
+            e2.FirstName = "yILMAZ";
+            e2.LastName = "Veli";
+            List.Add(e2);
+
+
+            return View(List);
+        }
+    }
+}
---
+++ NRM1_CoreMVC1/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NRM1_CoreMVC1.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Test()
+        {
+            //işlemler
+            return View();
+        }
+    }
+}
